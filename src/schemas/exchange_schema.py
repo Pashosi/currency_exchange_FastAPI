90ba4750ac58270e43f3cfb4273e7eba11ef438a
Pavel Bramanis <Bramanis2@gmail.com>
2025-02-10 19:03:58 +0300

    fix: Изменение полей схемы обмены валют

--- src/schemas/exchange_schema.py
+++ src/schemas/exchange_schema.py
@@ -8,7 +8,7 @@ class ExchangeSchema(BaseModel):
     base_currency: CurrencySchema = Field(..., title="baseCurrency")
     target_currency: CurrencySchema = Field(..., title="targetCurrency")
     rate: Decimal = Field(max_digits=9, decimal_places=6)
-    amount: Decimal = Field(max_digits=9, decimal_places=6)
-    converted_amount: Decimal = Field(max_digits=9, decimal_places=6, serialization_alias="convertedAmount")
+    amount: Decimal = Field(max_digits=100, decimal_places=6)
+    converted_amount: Decimal = Field(max_digits=100, decimal_places=6, serialization_alias="convertedAmount")
 
     model_config = ConfigDict(from_attributes=True)  # Позволяет преобразовывать SQLAlchemy объекты в Pydantic
